30da756d507f768d8d72217abe1962c6d12f000f
yoshoku <yoshoku@outlook.com>
2025-06-10 00:23:04 +0900

    chore: add basically implementation

--- examples/flet_svg_example/src/main.py
+++ examples/flet_svg_example/src/main.py
@@ -7,14 +7,12 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    page.add(
-
-                ft.Container(height=150, width=300, alignment = ft.alignment.center, bgcolor=ft.Colors.PURPLE_200, content=FletSvg(
-                    tooltip="My new FletSvg Control tooltip",
-                    value = "My new FletSvg Flet Control", 
-                ),),
-
-    )
-
+    page.add(ft.Container(
+        height=150,
+        width=300,
+        alignment=ft.alignment.center,
+        bgcolor=ft.Colors.PURPLE_200,
+        content=FletSvg()
+    ))
 
 ft.app(main)
--- src/flutter/flet_svg/lib/src/flet_svg.dart
+++ src/flutter/flet_svg/lib/src/flet_svg.dart
@@ -1,5 +1,6 @@
 import 'package:flet/flet.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class FletSvgControl extends StatelessWidget {
   final Control? parent;
@@ -13,9 +14,12 @@ class FletSvgControl extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String text = control.attrString("value", "")!;
-    Widget myControl = Text(text);
-
+    const String svgUrl =
+      'https://raw.githubusercontent.com/flet-dev/examples/refs/heads/main/python/tutorials/solitaire/solitaire-game-rules/assets/images/King_clubs.svg';
+    Widget myControl = SvgPicture.network(
+      svgUrl,
+      semanticsLabel: 'King of Clubs',
+    );
 
     return constrainedControl(context, myControl, parent, control);
   }
